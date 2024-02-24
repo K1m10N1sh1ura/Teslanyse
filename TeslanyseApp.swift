87f32ae36f04db7ad721fb454b057795710201c9
Kimio Nishiura <kimio.nishiura@me.com>
2024-02-24 15:39:28 +0100

    initial commit

---
+++ TeslanyseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslanyseApp.swift
+//  Teslanyse
+//
+//  Created by Kimio Nishiura on 24.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TeslanyseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
