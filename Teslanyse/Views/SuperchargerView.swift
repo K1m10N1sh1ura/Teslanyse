427614bacc7fd1e53154c58afa39215b187134fc
Kimio Nishiura <kimio.nishiura@me.com>
2024-03-03 16:28:54 +0100

    minor changes

--- Teslanyse/Views/SuperchargerView.swift
+++ Teslanyse/Views/SuperchargerView.swift
@@ -12,7 +12,7 @@ struct SuperchargerView: View {
     @StateObject var plotDataViewModel: PlotDataViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
