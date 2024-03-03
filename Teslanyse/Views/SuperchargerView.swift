1ecb02a50f75f1e28a95449a10081f6b1feb5768
Kimio Nishiura <kimio.nishiura@me.com>
2024-03-03 16:17:16 +0100

    add superchargerView in startscreenView List

---
+++ Teslanyse/Views/SuperchargerView.swift
@@ -0,0 +1,21 @@
+//
+//  SuperchargerView.swift
+//  Teslanyse
+//
+//  Created by Kimio Nishiura on 01.03.24.
+//
+
+import SwiftUI
+
+struct SuperchargerView: View {
+    
+    @StateObject var plotDataViewModel: PlotDataViewModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperchargerView(plotDataViewModel: plotDataViewModel)
+}
